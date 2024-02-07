03217a96230a0a18d69bf54e6a1831b72fe16b38
Aurora S <anatagora@gmail.com>
2024-02-07 12:44:40 +0100

    Ejercicio 8 UD

---
+++ TA5-FlujoDeDatos/src/Ejercicio8TAApp.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio8TAApp {
+
+	public static void main(String[] args) {
+	/**
+	 * Muestra los números del 1 al 100 (ambos incluidos).
+	 * Usa un bucle for. 
+	 */
+	for(int num=1; num<101; num++) {
+		System.out.println(num);
+	}
+		
+	}
+
+}
