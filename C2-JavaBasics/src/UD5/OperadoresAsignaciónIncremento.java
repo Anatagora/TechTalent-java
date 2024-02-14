e74a342d1a0e5ad8ceef0665391748907973d638
Aurora S <anatagora@gmail.com>
2024-02-14 13:13:49 +0100

    actualización
    Actualización de archivos

--- C2-JavaBasics/src/UD5/OperadoresAsignaciónIncremento.java
+++ C2-JavaBasics/src/UD5/OperadoresAsignaciónIncremento.java
@@ -1,4 +1,4 @@
-package pruebas;
+package UD5;
 
 public class OperadoresAsignaciónIncremento {
 
--- C2-JavaBasics/src/UD6/T6Ejercicio1App.java
+++ C2-JavaBasics/src/UD6/T6Ejercicio1App.java
@@ -1,32 +1,55 @@
 package UD6;
 
+import javax.swing.JOptionPane;
+
 public class T6Ejercicio1App {
 
 	public static void main(String[] args) {
-	
-	}
-	
-	public static int figura() {
-		String resultado= ""; 
 		
-		switch () {
-		case circulo: 
-			scanner 
+		String poligono= JOptionPane
+				.showInputDialog("Introduce un polígono");
+		
+		switch (poligono.toLowerCase()) {
+		case "circulo":
+			// 1.Crear variable(s) de parametro(s)
+			// 2. Pedir los valores
+			double var1= Double.parseDouble(JOptionPane.showInputDialog("Dame el radio: ")); 
+			
+			// 3. Llamar al método
+			double resultado=circulo(var1); 
+			System.out.println("El resultado del cálculo es: " +resultado);
+			
 			break; 
-		case triangulo: 
-			scanner
-			scanner 
+		case "triangulo": 
+			double var2=Double.parseDouble(JOptionPane.showInputDialod("Dame la base"));
+			double var3=;
 			break; 
-		case cuadrado: 
-			scanner
+		
+		case "cuadrado": 
+			
 			break; 
+			
 		default: 
-			resultado = "Figura no valida"; 
-		
-		
+			System.out.println("Esto no es un polígono");
+			break; 
 		}
 	
 	}
-	public static int circulo()
-
+	
+	public static double circulo(double radio) {
+		return(radio*(radio))*Math.PI; 
+	
+	}
+	public static double triangulo (double base, double altura) {
+		return(base*altura)/2; 
+	}
+	public static double cuadrado(double lado) {
+		return(lado*lado); 
+	
+	}
+		
+	
 }
+	
+
+
