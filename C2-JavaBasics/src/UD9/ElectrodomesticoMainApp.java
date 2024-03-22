0a0f80f73451496454a230fb86d6d0740085a889
Aurora S <anatagora@gmail.com>
2024-03-22 11:31:34 +0100

    UD9 ejercicio 1
    -Instanciaciones creadas y añadidas a cada posición del array
    
    -Cambio de Electrodoméstico a clase abstract.
    
    - Creación de un for-each que recorra el array.

--- C2-JavaBasics/src/UD9/ElectrodomesticoMainApp.java
+++ C2-JavaBasics/src/UD9/ElectrodomesticoMainApp.java
@@ -2,33 +2,46 @@ package UD9;
 
 public class ElectrodomesticoMainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		ElectrodomesticoUD9 ArrayE[]= new ElectrodomesticoUD9 [10]; 
+		 
 		
-		
-		
-		// Instanciacion Electrodomestico Clase Padre 
-		
-		ElectrodomesticoUD9 electrodomestico1 = new ElectrodomesticoUD9(); 
-		electrodomestico1.mostrar();
-		
-		ElectrodomesticoUD9 electrodomestico2 = new ElectrodomesticoUD9(105, 32);
-		electrodomestico2.mostrar();
-		
-		ElectrodomesticoUD9 electrodomestico3 = new ElectrodomesticoUD9(125, "Verde", 'A', 81);
-		electrodomestico3.mostrar();
-		
-		ElectrodomesticoUD9 electrodomestico4 = new ElectrodomesticoUD9(200, "Amarillo", 'J', 25);
-		electrodomestico4.mostrar();
-		System.out.println("\n");
-		
-		// Sub-clase Lavadora
+		// clase hija Lavadora
 		
 		Lavadora Lavadora1 = new Lavadora(200, "Amarillo", 'A', 25,35);
+		Lavadora Lavadora2 = new Lavadora(200, "Rojo", 'C', 80,25);
+		Lavadora Lavadora3 = new Lavadora(200, "Gris", 'B', 85,30);
+		Lavadora Lavadora4 = new Lavadora(200, "Azul", 'B', 70,15);
+		Lavadora Lavadora5 = new Lavadora(200, "Blanco", 'B', 85,30);
+		
 		Lavadora1.mostrar();
 		System.out.println("\n");
 		
+		//clase hija Televisión 
+		Television Television1 = new Television (100, "Gris", 'A', 25, 60,false);
+		Television Television2 = new Television (105, "Blanco", 'F', 25, 32,false);
+		Television Television3 = new Television (105, "Rojo", 'C', 25, 20,false);
+		Television Television4 = new Television (105, "Rojo", 'C', 25, 20,false);
+		Television Television5 = new Television (105, "Rojo", 'C', 25, 20,false);
+		
+		ArrayE[0] = Lavadora4; 
+		ArrayE[1] = Lavadora5; 
+		ArrayE[2] = Television4; 
+		ArrayE[3] = Television5; 
+		ArrayE[4] = Lavadora1; 
+		ArrayE[5] = Lavadora2; 
+		ArrayE[6] = Lavadora3; 
+		ArrayE[7] = Television1; 
+		ArrayE[8] = Television2; 
+		ArrayE[9] = Television3; 
+		
+		for (ElectrodomesticoUD9 electrodomesticoUD9 : ArrayE) 
+		{
+			ElectrodomesticoUD9.precioFinal(); 
+		}
+	
 
 	}
 
