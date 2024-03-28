9170f60ff8f8aa2ca655f925fe30586a5273a73a
Aurora S <anatagora@gmail.com>
2024-03-28 12:26:12 +0100

    Main ej4 -ud9
    Creación del main del ejercicio 4 de UD9
    falta comprobar datos correctos

--- C2-JavaBasics/src/UD9/Ejercicio4/Main.java
+++ C2-JavaBasics/src/UD9/Ejercicio4/Main.java
@@ -1,9 +1,19 @@
 package UD9.Ejercicio4;
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		// No lo pide, pero hago un ejemplo de la usabilidad del código.
+		Raices ecuacion =new Raices(1,-3,2); 
+		System.out.println("¿Discriminante? " +ecuacion.getDiscriminante());
+		System.out.println("¿Tiene raíces? "+ecuacion.tieneRaices() );
+		System.out.println("¿Tiene una única raíz? " +ecuacion.tieneRaiz());
 		
+		ecuacion.calcular();
+		ecuacion.obtenerRaices();
+		ecuacion.obtenerRaiz();
 
 	}
 
