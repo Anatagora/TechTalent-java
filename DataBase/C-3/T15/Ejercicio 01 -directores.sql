9532c8e622b7e74f193c71af530c0f8273f7e464
Aurora S <anatagora@gmail.com>
2024-04-16 11:47:12 +0200

    Actu nombres

---
+++ DataBase/C-3/T15/Ejercicio 01 -directores.sql
@@ -0,0 +1,16 @@
+INSERT INTO despachos (capacidad) VALUES
+(3),(8),(7),(6),(4),(1),(2),(9),(5),(12);
+
+--Inicial prueba 
+
+INSERT INTO directores (dni, nom_apels, dni_jefe, num_despacho) VALUES
+('12345', 'Socorro Ruiz, Aurora', NULL, '2'), 
+('22345', 'Sanchez, Pedro', NULL, '3'), 
+('32345', 'Rajoy, Mariano', NULL, '2'), 
+('42345', 'Swift, Taylor', NULL, '2'), 
+('62345', 'Chalamet, Timothée', NULL, '2'), 
+('52345', 'Shelby, Tommy', NULL, '2'), 
+('72345', 'White, Walter', NULL, '2'), 
+('82345', 'Monkey D, Luffy', NULL, '2'), 
+('92345', 'Roronoa, Zoro', NULL, '2'), 
+('13345', 'Lee,Bruce ', NULL, '2'), 
