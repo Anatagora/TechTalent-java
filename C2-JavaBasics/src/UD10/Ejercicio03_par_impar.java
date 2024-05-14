0aa7c995825dd6681266b386bd2108f9bc7fed37
Aurora S <anatagora@gmail.com>
2024-05-14 12:38:20 +0200

    T18 - ejercicio 1

--- C2-JavaBasics/src/UD10/Ejercicio03_par_impar.java
+++ C2-JavaBasics/src/UD10/Ejercicio03_par_impar.java
@@ -3,47 +3,45 @@ package UD10;
 import java.util.Random;
 
 //Definición de la excepción personalizada para números pares
-class NumeroParException extends Exception 
-{
-  public NumeroParException(String message) {
-      super(message);
-  }
-}
+	class NumeroParException extends Exception 
+	{
+	public NumeroParException(String message) 
+		{
+		super(message);
+		}
+	}
 
 //Clase principal que genera un número aleatorio y determina si es par o impar
 
-class Ejercicio03_par_impar 
-{
-  // Método para generar un número aleatorio entre 0 y 999
-	
-  public int generarNumeroAleatorio() {
-      Random rand = new Random();
-      return rand.nextInt(1000); // Se generará un número entre 0 y 999
-  	}
+	class Ejercicio03_par_impar {
+	// Método para generar un número aleatorio entre 0 y 999
 
-  // Método para determinar si un número es par o impar
-  public void determinarParidad(int numero) throws NumeroParException {
-      if (numero % 2 == 0) {
-          throw new NumeroParException("El número generado es par: " + numero);
-      } else {
-          System.out.println("El número generado es impar: " + numero);
-      }
-  }
+	public int generarNumeroAleatorio() {
+		Random rand = new Random();
+		return rand.nextInt(1000); // Se generará un número entre 0 y 999
+	}
 
-  // Método main dentro de la clase Ejercicio03_par_impar
-  public static void main(String[] args) 
-  {
-      Ejercicio03_par_impar generador = new Ejercicio03_par_impar();
+	// Método para determinar si un número es par o impar
+	public void determinarParidad(int numero) throws NumeroParException 
+	{
+		if (numero % 2 == 0) {
+			throw new NumeroParException("El número generado es par: " + numero);
+		} else {
+			System.out.println("El número generado es impar: " + numero);
+		}
+	}
 
-      try {
-          int numeroAleatorio = generador.generarNumeroAleatorio();
-          generador.determinarParidad(numeroAleatorio);
-      } catch (NumeroParException e) {
-          System.out.println(e.getMessage());
-      }finally {
-      	System.out.println("Programa finalizado!");
-      }
-  }
+	// Método main dentro de la clase Ejercicio03_par_impar
+	public static void main(String[] args) {
+		Ejercicio03_par_impar generador = new Ejercicio03_par_impar();
+
+		try {
+			int numeroAleatorio = generador.generarNumeroAleatorio();
+			generador.determinarParidad(numeroAleatorio);
+		} catch (NumeroParException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("Programa finalizado!");
+		}
+	}
 }
-
-
---
+++ C2-JavaBasics/src/UD18_JDBC_conexion/Ejercicio01_Tienda_Inform.java
@@ -0,0 +1,79 @@
+package UD18_JDBC_conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Ejercicio01_Tienda_Inform {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306?useTimezone=true&serverTimezone=UTC", "root", "");
+            System.out.println("Conexión establecida con éxito");
+
+            // Crear la base de datos
+            try (Statement stmt = conexion.createStatement()) {
+                String createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS Tienda_Infor_01";
+                stmt.executeUpdate(createDatabaseQuery);
+                System.out.println("Base de datos creada o ya existente: Tienda_Infor_01");
+
+                // Usar la base de datos
+                String useDatabaseQuery = "USE Tienda_Infor_01";
+                stmt.executeUpdate(useDatabaseQuery);
+                System.out.println("Usando la base de datos: Tienda_Infor_01");
+
+                // Crear la tabla fabricantes
+                String createFabricantesTableQuery = "CREATE TABLE IF NOT EXISTS fabricantes (" +
+                        "cod_fabricantes INT, " +
+                        "nombre VARCHAR(100), " +
+                        "PRIMARY KEY (cod_fabricantes)" +
+                        ")";
+                stmt.executeUpdate(createFabricantesTableQuery);
+                System.out.println("Tabla creada o ya existente: fabricantes");
+
+                // Insertar registros en la tabla fabricantes
+                String insertFabricantesQuery = "INSERT IGNORE INTO fabricantes (cod_fabricantes, nombre) VALUES " +
+                        "(01, 'Leroy Merlin'), " +
+                        "(02, 'Ikea'), " +
+                        "(03, 'Bauhaus'), " +
+                        "(04, 'Bricomart'), " +
+                        "(05, 'Fesmes')";
+                stmt.executeUpdate(insertFabricantesQuery);
+                System.out.println("Registros insertados en la tabla fabricantes");
+
+                // Crear la tabla articulos
+                String createArticulosTableQuery = "CREATE TABLE IF NOT EXISTS articulos (" +
+                        "cod_articulos INT, " +
+                        "nombre VARCHAR(100), " +
+                        "precio INT, " +
+                        "cod_fabricantes INT, " +
+                        "PRIMARY KEY (cod_articulos), " +
+                        "FOREIGN KEY (cod_fabricantes) " +
+                        "REFERENCES fabricantes(cod_fabricantes) " +
+                        "ON DELETE SET NULL " +
+                        "ON UPDATE CASCADE" +
+                        ")";
+                stmt.executeUpdate(createArticulosTableQuery);
+                System.out.println("Tabla creada o ya existente: articulos");
+
+                // Insertar registros en la tabla articulos
+                String insertArticulosQuery = "INSERT IGNORE INTO articulos (cod_articulos, nombre, precio, cod_fabricantes) VALUES " +
+                        "(1, 'Tornillo', 100, 01), " +
+                        "(2, 'Arandela', 150, 02), " +
+                        "(3, 'Tuerca', 200, 03), " +
+                        "(4, 'Brida', 120, 04), " +
+                        "(5, 'Masilla', 180, 05)";
+                stmt.executeUpdate(insertArticulosQuery);
+                System.out.println("Registros insertados en la tabla articulos");
+            } catch (SQLException e) {
+                System.err.println("Error al ejecutar las consultas SQL: " + e.getMessage());
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("No se ha podido conectar con la base de datos");
+            ex.printStackTrace();
+        }
+    }
+}
+
