2d5afb267db4c7ca7bc1091cf13e9b95d692b069
Aurora S <anatagora@gmail.com>
2024-05-29 11:01:54 +0200

    UD22 - EJERCICIO 1

---
+++ C4-Maven/src/main/java/UD22_MVC/modelo/Cliente.java
@@ -0,0 +1,71 @@
+package UD22_MVC.modelo;
+
+import java.sql.Date;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String direccion;
+    private String dni;
+    private Date fecha;
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
+
---
+++ C4-Maven/src/main/java/UD22_MVC/modelo/Video.java
@@ -0,0 +1,42 @@
+package UD22_MVC.modelo;
+
+public class Video {
+    private int id;
+    private String title;
+    private String director;
+    private int cli_id;
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getCli_id() {
+        return cli_id;
+    }
+
+    public void setCli_id(int cli_id) {
+        this.cli_id = cli_id;
+    }
+}
+
