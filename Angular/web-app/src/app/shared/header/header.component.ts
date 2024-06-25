4c7b2b14111cfcb5ca7645b0f5d3e7d2ea5aed98
Aurora S <anatagora@gmail.com>
2024-06-25 13:04:57 +0200

    footer

---
+++ Angular/web-app/src/app/shared/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']  // Corrección aquí
+})
+export class HeaderComponent {
+
+}
