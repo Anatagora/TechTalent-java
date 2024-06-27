4956d51a1d84ef469bff0e383228303f47c672fa
Aurora S <anatagora@gmail.com>
2024-06-27 09:56:56 +0200

    angular

--- Angular/web-app/src/app/app.component.ts
+++ Angular/web-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TermsComponent } from './pages/terms/terms.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, TermsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, TermsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Angular/web-app/src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styles: ``
+})
+export class PortfolioComponent {
+
+}
