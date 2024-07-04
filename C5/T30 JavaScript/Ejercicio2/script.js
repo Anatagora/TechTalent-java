57ebe764a8761174feca46476b6fe418f14a0275
Aurora S <anatagora@gmail.com>
2024-07-04 10:58:31 +0200

    javascript I acabado

---
+++ C5/T30 JavaScript/Ejercicio2/script.js
@@ -0,0 +1,19 @@
+// codigo.js
+
+// Este script muestra dos mensajes en la consola
+
+// Almacenamos el mensaje en una variable
+var mensaje1 = "Hola Mundo!";
+var mensaje2 = "Soy el primer script";
+
+// Mostramos el primer mensaje
+console.log(mensaje1);
+
+// Mostramos el segundo mensaje
+console.log(mensaje2);
+
+// Almacenamos el mensaje complejo en una variable
+var mensajeComplejo = "Hola Mundo!\nQué fácil es incluir 'comillas simples' y \"comillas dobles\"";
+
+// Mostramos el mensaje complejo en una ventana de alerta
+alert(mensajeComplejo);
---
+++ C5/T30 JavaScript/Ejercicio3/script.js
@@ -0,0 +1,3 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses);
